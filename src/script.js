087665dafca1577f193b221eb2ce817c67e823c8
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-10-11 13:46:01 -0700

    Trying to make stuff work

--- src/script.js
+++ src/script.js
@@ -18,19 +18,60 @@ function showDate() {
 
   document.querySelector("#date-time").innerHTML = `${day}, ${hour}:${minute}`;
 }
-function changeCity() {
-  let citySearch = document.getElementById("city-input");
-  citySearch = citySearch.trim();
-  citySearch = citySearch.toLowerCase();
+function showTemp(response) {
+  let highTemp = Math.round(response.data.main.temp_max);
+  let lowTemp = Math.round(response.data.main.temp_min);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    `${response.data.main.temp}°`
+  );
+  document.querySelector(".feels-like").innerHTML = Math.round(
+    `Feels Like: ${response.data.main.feels_like}°`
+  );
+  document.querySelector(
+    ".high-low-main"
+  ).innerHTML = `H: ${highTemp}° L: ${lowTemp}°`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector(
+    "#precipitation"
+  ).innerHTML = `Precipitation: ${response.data.weather[0].main}`;
+  document.querySelector(
+    "#aqi"
+  ).innerHTML = `Wind Speed:${response.data.wind.speed}/mph`;
 
-  let unit = "imperial";
-  let apiKey = "f94ea25a0e5f5ee54c0ba93fee57f24d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&appid=${apiKey}&units=${unit}`;
-
-  axios.get(apiUrl).then(showTemp);
+  console.log(response);
 }
+function defaultWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    `${response.data.main.temp}°`
+  );
+
+  document.querySelector(".feels-like").innerHTML = Math.round(
+    `Feels Like: ${response.data.main.feels_like}°`
+  );
+  document.querySelector(
+    ".high-low-main"
+  ).innerHTML = `H: ${highTemp}° L: ${lowTemp}°`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector(
+    "#precipitation"
+  ).innerHTML = `Precipitation: ${response.data.weather[0].main}`;
+  document.querySelector(
+    "#aqi"
+  ).innerHTML = `Wind Speed:${response.data.wind.speed}/mph`;
+}
+function submitInfo(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector("#city-input").value;
+  changeCity(citySearch);
+}
+
 function showPosition(position) {
-  navigator.geolocation.getCurrentPosition(showPosition);
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let unit = "imperial";
@@ -40,32 +81,16 @@ function showPosition(position) {
 
   axios.get(apiUrl).then(showTemp);
 }
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+function changeCity(citySearch) {
+  let unit = "imperial";
+  let apiKey = "f94ea25a0e5f5ee54c0ba93fee57f24d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=${unit}`;
 
-function showTemp(response) {
-  let city = response.data.name;
-  let mainTemp = Math.round(response.data.main.temp);
-  let feelsLike = Math.round(response.data.main.feels_like);
-  let highTemp = Math.round(response.data.main.temp_max);
-  let lowTemp = Math.round(response.data.main.temp_min);
-  let humidity = response.data.main.humidity;
-  let precipitation = response.data.weather[0].main;
-  let windSpeed = response.data.wind.speed;
-  let header = document.querySelector("#city");
-  let temp = document.querySelector("#temp");
-  let feel = document.querySelector(".feels-like");
-  let highLow = document.querySelector(".high-low-main");
-  let humidityDisplay = document.querySelector("#humidity");
-  let precipitationDisplay = document.querySelector("#precipitation");
-  let windSpeedDisplay = document.querySelector("#aqi");
-  header.innerHTML = `${city}`;
-  temp.innerHTML = `${mainTemp}°`;
-  feel.innerHTML = `Feels Like: ${feelsLike}°`;
-  highLow.innerHTML = `H: ${highTemp}° L: ${lowTemp}°`;
-  humidityDisplay.innerHTML = `Humidity: ${humidity}%`;
-  precipitationDisplay.innerHTML = `Precipitation: ${precipitation}`;
-  windSpeedDisplay.innerHTML = `Wind Speed: ${windSpeed}/mph`;
-
-  console.log(response);
+  axios.get(apiUrl).then(showTemp);
 }
 
 //function toCelcius() {
@@ -79,14 +104,18 @@ function showTemp(response) {
 //
 //  display.innerHTML = "57°F";
 //}
-let cityButton = document.querySelector("#search-button");
-let currentButton = document.querySelector("#current-button");
+
 //let celcius = document.querySelector(".c-link");
 //let farenheit = document.querySelector(".f-link");
 
 //celcius.addEventListener("click", toCelcius);
 //farenheit.addEventListener("click", toFarenheit);
 
-cityButton.addEventListener("click", changeCity);
-currentButton.addEventListener("click", showPosition);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", submitInfo);
+
+let currentButton = document.querySelector("#current-button");
+currentButton.addEventListener("click", getLocation);
+
 showDate();
+defaultWeather("Seattle");
